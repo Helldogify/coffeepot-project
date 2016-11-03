f37ed18f4c37deb1f63f795c62e7bef38b412b92
mahipaul tak <i.no.u.ok@gmail.com>
2016-11-02 21:51:07 -0600

    Update MyWaterControlCode_ASM.asm

--- MyWaterControlCode_ASM.asm
+++ MyWaterControlCode_ASM.asm
@@ -20,20 +20,20 @@ LINK 20;
 	R1 =R0; // R1 = current water level
 WHILE_WCC:
 
-CC = R1 <= R5;
+CC = R1 <= R5; // Check if waterlevel is at the required amount 
 
-If !CC JUMP END_WHILE_CC;
-P0 = R4;
-R2 = 20; 
-       B[P0 + 0x7] = R2;
+If !CC JUMP END_WHILE_CC; // if it is stop adding water
+P0 = R4; //P0 = coffeepot baseregister
+R2 = 20; // amount of water to be added per second (determined through experiments)
+       B[P0 + 0x7] = R2; // water inflow register = 20
        
        .extern __Z31My_SimulateOneSecondPassing_CPPv;
-       CALL __Z31My_SimulateOneSecondPassing_CPPv; 
+       CALL __Z31My_SimulateOneSecondPassing_CPPv; //wait a 1 second
        
-       R0 = R4;
+       R0 = R4; // put base register value in current water level function argument
        .extern __Z21CurrentWaterLevel_CPPP16COFFEEPOT_DEVICE; 
        CALL __Z21CurrentWaterLevel_CPPP16COFFEEPOT_DEVICE; 
-       R1 = R0; 
+       R1 = R0; // update current water level
        JUMP WHILE_WCC;
 END_WHILE_CC:
 _WaterControlCode_ASM.END:
